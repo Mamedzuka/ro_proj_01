dcd6d9206a6a3ade02159d839b6267163794e58e
Мамед <>
2022-01-28 16:37:45 +0300

    Better tables More functions

--- proj.py
+++ proj.py
@@ -2,12 +2,11 @@ from math import log, sin, cos, fabs
 
 NUM = 128
 
-def even_max(value):
-
-    def even_lhs():
+def even_lhs(value):
         res_value = sin(value + NUM)
         if res_value == 0:
             return f"Error: devision zero"
+       
         res_value = ((1-NUM) / res_value)
         if res_value < 0:
             return f"Error: log argument < 0"
@@ -15,65 +14,73 @@ def even_max(value):
             return f"Error: log argument = 0"
         return log(res_value, 21)
 
-    def even_rhs():
-        return fabs(cos(value) / NUM)
+def even_rhs(value):
+    return fabs(cos(value) / NUM)
 
-    lhs = even_lhs()
+def even_max(value):
+    lhs = even_lhs(value)
     if isinstance(lhs, str):
         return lhs
     else:
-        return max(lhs, even_rhs())
+        return max(lhs, even_rhs(value))
+
+def odd_lhs(value):
+    res_value = cos(value - NUM)
+    if res_value == 0:
+        return f"Error: devision zero"
+    res_value = ((1-NUM) / res_value)
+    if res_value < 0:
+        return f"Error: log argument < 0"
+    elif res_value == 0:
+        return f"Error: log argument = 0"
+    return log(res_value, 21)
+
+def odd_rhs(value):
+    return sin(value) / NUM
 
 def odd_min(value):
-    
-    def odd_lhs():
-        res_value = cos(value - NUM)
-        if res_value == 0:
-            return f"Error: devision zero"
-        res_value = ((1-NUM) / res_value)
-        if res_value < 0:
-            return f"Error: log argument < 0"
-        elif res_value == 0:
-            return f"Error: log argument = 0"
-        return log(res_value, 21)
-
-    def odd_rhs():
-        return sin(value) / NUM
-    
-    lhs = odd_lhs()
+    lhs = odd_lhs(value)
     if isinstance(lhs, str):
         return lhs
     else:
-        return min(lhs, odd_rhs())
+        return min(lhs, odd_rhs(value))
 
 def table_former(init_val, fin_val, step):
-    result_tab = open('result_tab.txt', 'w')
-    
-    str_step = str(step)
-    if step%1 == 0:
-        dec_places = 0
-    elif str_step.find('.') != -1:
-        dec_places = len(str_step.split('.')[1])
-    else:
-        dec_places = 5
-    
-    counter = 1
-
-    print(f'{"Number":<10}{"Value":<{dec_places + 10}}{"Result":<40}')
-    print(f'{"Number":<10}{"Value":<{dec_places + 10}}{"Result":<40}',
-                                                        file=result_tab)
-
-    while init_val <= fin_val:
-        if (counter%2 == 0):
-            result = even_max(init_val)
+    def dec_placer():
+        str_step = str(step)
+        if step%1 == 0:
+            return 0
+        elif str_step.find('.') != -1:
+            return len(str_step.split('.')[1])
         else:
-            result = odd_min(init_val)
-        print(f'{counter:<10}{init_val:<{dec_places + 10}.{dec_places}f}{result:<40}')
-        print(f'{counter:<10}{init_val:<{dec_places + 10}.{dec_places}f}{result:<40}',
-                                                                        file=result_tab)
-        counter, init_val = counter + 1, init_val + step
+            return 5
 
-    result_tab.close()  
+    with open('result_tab.txt', 'w') as result_tab:
+        dec_places = dec_placer()
+        
+        counter = 1
+    
+        print('-' * (54 + dec_places))
+        print(f'|{"Number":^10}|{"Value":^{dec_places + 10}}|{"Result":^30}|')
+        print('-' * (54 + dec_places))
+        print('-' * (54+dec_places), file=result_tab)
+        print(f'|{"Number":^10}|{"Value":^{dec_places + 10}}|{"Result":^30}|',
+                                                            file=result_tab)
+        print('-' * (54+dec_places), file=result_tab)
+
+        while init_val <= fin_val:
+            if (counter%2 == 0):
+                result = even_max(init_val)
+            else:
+                result = odd_min(init_val)
+            print(f'|{counter:>10}|{init_val:>{dec_places + 10}.{dec_places}f}|{result:>30}|')
+            print(f'|{counter:>10}|{init_val:>{dec_places + 10}.{dec_places}f}|{result:>30}|',
+                                                                            file=result_tab)
+            counter += 1
+            init_val += step 
+
+        print('-' * (54 + dec_places))
+        print('-' * (54+dec_places), file=result_tab)
 
 def main():
     while True:    
@@ -83,10 +90,9 @@ def main():
             if initial >= final:
                 raise ValueError("initial value >= final value")
             step = float(input("Input sequence step: "))
-            if step <= 0:
-                raise ValueError("step value <= 0")
+            if step == 0:
+                raise ValueError("step value = 0")
             break
-            
         except Exception as excpt:
             print("Error: " + excpt.args[0], end='\n\n')
 
